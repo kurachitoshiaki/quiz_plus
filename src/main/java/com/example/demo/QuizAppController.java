d6f5af60369322a93a4b0a889f36bcc83eb2d579
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2021-03-06 13:25:01 +0900

    Update QuizAppController.java

--- src/main/java/com/example/demo/QuizAppController.java
+++ src/main/java/com/example/demo/QuizAppController.java
@@ -10,16 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class QuizAppController {
-	private List<String> quizzes = new ArrayList<>();
+	private List<Quiz> quizzes = new ArrayList<>();
 	
+	// クイズ一覧
 	@GetMapping("/show")
-	public List<String> show() {
+	public List<Quiz> show() {
 		return quizzes;
 	}
 	
+	//　クイズ作成
 	@PostMapping("/create")
 	public void create(@RequestParam String question, @RequestParam boolean answer) {
-		String quiz = question +  ":" + answer;
+		Quiz quiz = new Quiz(question, answer);
 		quizzes.add(quiz);
 	}
 }
