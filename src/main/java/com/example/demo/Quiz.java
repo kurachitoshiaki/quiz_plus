b409b034431cec4e42dd774e7d0bbeaaeb3a4081
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2021-03-06 13:52:14 +0900

    Update Quiz.java

--- src/main/java/com/example/demo/Quiz.java
+++ src/main/java/com/example/demo/Quiz.java
@@ -11,17 +11,33 @@ public class Quiz {
 	 * クイズの正解
 	 */
 	private boolean answer;
+	private int fourAnswer;
 	
 	public Quiz(String question, boolean answer) {
 		this.question = question;
 		this.answer = answer;
 	}
 	
+	public Quiz(String question, int fourAnswer) {
+		this.question = question;
+		this.fourAnswer = fourAnswer;
+	}
+	
 	public String getQuestion() {
 		return question;
 	}
-	
+	// 二択問題
 	public boolean isAnswer() {
 		return answer;
+	}
+	// 四択問題
+	public int isFourAnswer(){
+		return fourAnswer;
+	}
+	
+	@Override
+	public String toString() {
+		String marubatsu = answer ? "○" : "×";
+		return question + "" + marubatsu;
 	}
 }
