856d6ec301c41c0b11d936986b8c89d8b6b38cf6
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2021-03-08 10:45:13 +0900

    Create QuizFileDao.java

---
+++ src/main/java/com/example/demo/QuizFileDao.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+// Data access object
+public class QuizFileDao{
+	
+	private static final String FILE_PATH = "quizzes.tex";
+	
+	public void write(List<Quiz> quizzes) throws IOException {
+		List<String> lines = new ArrayList<>();
+		
+		for(Quiz quiz: quizzes) {
+			lines.add(quiz.toString());
+		}
+		Path path = Paths.get(FILE_PATH);
+		Files.write(path, lines);
+	}
+}
